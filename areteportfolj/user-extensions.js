9c5f7a00a3bffb33f360f7aed320dee450239358
Andreas Ek <andreas@flowcom.se>
2013-04-29 09:40:59 +0200

    user-extention as a file instead

---
+++ areteportfolj/user-extensions.js
@@ -0,0 +1,8 @@
+
+/* these variables is used inside text cases */
+
+var minarete_password='MINARETE_PASSWORD';
+
+var reponse_name='UI-TEST'+Math.floor(Math.random()*111111111);
+
+var access_code='TEST'+Math.floor(Math.random()*111111111);
